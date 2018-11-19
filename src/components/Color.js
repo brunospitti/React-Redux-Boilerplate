89bb5477b45a3f06c01f335627a13924a6dee7f4
Bruno Spitti <bruno.olivares@live.com>
2018-11-19 19:56:38 +0000

    Done!

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,28 +1,70 @@
-import React from 'react';
+import React from "react";
 import styled from "react-emotion";
+import { connect } from "react-redux";
+import { darken, rgba } from "polished";
 
-import { Button } from './Button';
+import changeColor from "../redux/actionCreators/changeColor";
 
+import { Button } from "./basics/Button";
 
 class Color extends React.Component {
-    render(){
-        const { color, changeColor } = this.props;
+  render() {
+    const { color, changeColor } = this.props;
 
-        const ColoredBg = styled("div")`
-            background-color: rgb(${color[0]}, ${color[1]}, ${color[2]});
-        `;
+    let colorRgb = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
 
-        return(
-            <div className="color-wrapper">
-                <ColoredBg>
-                    <div className="color-rgb">
-                        RGB( {color.map( (colorU, i) => <span key={i}>{ colorU }{ (color.length !== i + 1) && ', ' }</span> )} )
-                    </div>
-                </ColoredBg>
-                <Button secondary text="Change color" clickBehav={changeColor(color)}/>
-            </div>
-        )
-    }
-};
+    const ColorWrapper = styled("div")`
+      background-color: ${colorRgb};
+      height: 250px;
+      margin: 0 auto;
+      text-align: center;
+      font-size: 1.2em;
+      .title {
+        background: ${rgba("white", 0.7)};
+        width: 100%;
+        padding: 20px;
+      }
+      .content {
+        padding: 40px;
+      }
+      button {
+        margin-top: 40px;
+        background-color: ${darken(0.2, colorRgb)};
+      }
+    `;
 
-export default Color;
+    return (
+      <ColorWrapper>
+        <div className="title">Randomize background-color by rgb values</div>
+        <div className="content">
+          <div>
+            RGB({" "}
+            {color.map((colorU, i) => (
+              <span key={i}>
+                {colorU}
+                {color.length !== i + 1 && ", "}
+              </span>
+            ))}{" "}
+            )
+          </div>
+          <Button text="Change color" clickBehaviour={changeColor} />
+        </div>
+      </ColorWrapper>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  color: state.color
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeColor(color) {
+    dispatch(changeColor(color));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Color);
--- src/components/Number.js
+++ src/components/Number.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+
+import addNum from "../redux/actionCreators/addNum";
+
+import { Button } from "./basics/Button";
 
 class Number extends React.Component {
-    render(){
-        const {num, addNum} = this.props;
-        return(
-            <div className="number-wrapper">
-                <p>
-                    <strong>Times the button has been clicked:</strong> {num}
-                </p>
-                <button
-                    className="btn increment-number"
-                    onClick={() => addNum(num) }
-                >
-                    Increment number
-                </button>
-            </div>
-        )
-    }
-};
+  render() {
+    const { num, addNum } = this.props;
+    return (
+      <div className="number-wrapper">
+        <p>
+          <strong>Times the button has been clicked:</strong> {num}
+        </p>
+        <Button text="Increment number" clickBehaviour={() => addNum(num)} />
+      </div>
+    );
+  }
+}
 
-export default Number;
+const mapStateToProps = state => ({
+  num: state.num
+});
+
+const mapDispatchToProps = dispatch => ({
+  addNum(num) {
+    dispatch(addNum(num));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Number);
